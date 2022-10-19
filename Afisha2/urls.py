06e871dffabecb47d6f570e2c8f8117159b1c214
teddyplay <kasymovablai8@gmail.com>
2022-10-19 06:23:11 +0600

    я сдела detail

--- Afisha2/urls.py
+++ Afisha2/urls.py
@@ -19,9 +19,18 @@ from movie_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+
     path('api/v1/directors/', views.directors),
+    path('api/v1/directors/<int:id>/', views.director_item),
+
+
     path('api/v1/movies/', views.movies),
+    path('api/v1/movies/<int:id>/', views.movie_item),
+
+
     path('api/v1/revies/', views.reviews),
+    path('api/v1/revies/<int:id>/', views.review_item),
 
 
 ]
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from movie_app.models import Director, Movie, Review
@@ -23,6 +24,37 @@ def movies(request):
 def reviews(request):
     reviews = Review.objects.all()
     data = ReviewSerializer(reviews, many=True).data
+    return Response(data=data)
+
+
+@api_view(["GET"])
+def director_item(request, id):
+    try:
+        director = Director.objects.get(id=id)
+    except Director.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    data = DirectorSerializer(director).data
+    return Response(data=data)
+
+
+@api_view(["GET"])
+def movie_item(request, id):
+    try:
+         movie = Movie.objects.get(id=id)
+    except Movie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    data = MovieSerializer(movie).data
+    return Response(data=data)
+
+
+
+@api_view(["GET"])
+def review_item(request, id):
+    try:
+        review = Review.objects.get(id=id)
+    except Review.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    data = ReviewSerializer(review).data
     return Response(data=data)
 
 
